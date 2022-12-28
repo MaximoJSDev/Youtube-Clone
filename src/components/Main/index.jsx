698a5adf0e1cc6722d845c4b158976c83f617abf
Maximo-4ever <maximilianocm06@gmail.com>
2022-12-28 14:59:44 -0600

    Añade todos los videos del inicio

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,295 @@
+import styles from "./main.module.css";
+
+function index() {
+  return (
+    <main className={styles.main}>
+      <header className={styles.main__header}>
+        <span className={styles.main__header__active}>Todos</span>
+        <span>Videojuegos</span>
+        <span>En tiempo real</span>
+        <span>Música</span>
+        <span>Mixes</span>
+        <span>Prgramación</span>
+        <span>Hardware</span>
+        <span>Podcast</span>
+        <span>Vistos</span>
+        <span>Publicaciones</span>
+        <span>Novedad para ti</span>
+      </header>
+      <div className={styles.main__contents}>
+        <div className={styles.main__contents__grid}>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=1"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=1"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=2"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=2"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=3"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=3"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=4"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=4"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.main__contents__grid}>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=5"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=5"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=6"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=6"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=7"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=7"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=8"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=8"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.main__contents__grid}>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=9"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=9"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=10"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=10"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=11"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=11"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.video}>
+            <img
+              className={styles.video__thumbnail}
+              src="https://picsum.photos/360/200?random=12"
+              alt=""
+            />
+            <div className={styles.video__details}>
+              <img
+                className={styles.video__details__avatar}
+                src="https://picsum.photos/50/50?random=12"
+                alt=""
+              />
+              <div className={styles.video__details__content}>
+                <h3>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Neque.
+                </h3>
+                <span>Mr. Beast</span>
+                <span>515,673 Vistas</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default index;
